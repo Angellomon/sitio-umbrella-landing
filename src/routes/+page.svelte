5a3b8c524c46acf6b7ae40deb216e8b49fd2fe50
Angellomon <angelmtzdiaz@gmail.com>
2023-06-06 18:54:53 -0600

    feat(test): impl terst

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section id="presentacion" />
+<section id="servicios" />
+<section id="innovacion" />
+<section id="marcas" />
+<section id="equipo" />
