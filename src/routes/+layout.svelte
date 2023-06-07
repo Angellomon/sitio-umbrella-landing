ed98717a85d587464ad4956f1e00894f6e5460ed
Angellomon <angelmtzdiaz@gmail.com>
2023-06-07 03:09:14 -0600

    feat(routes): impl layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,48 @@
 <script lang="ts">
 	import '../app.css';
+	import InteractiveCursor from '../lib/components/interactive-cursor/interactive-cursor.svelte';
+	import { size, coords } from '$lib/store';
+
+	const handleMouseMove = (e: MouseEvent) => {
+		$coords = { x: e.clientX, y: e.clientY };
+	};
+
+	const handleMouseDown = () => {
+		$size = 20;
+	};
+
+	const handleMouseUp = () => {
+		$size = 10;
+	};
 </script>
 
-<header>
-	<nav>
-		<ul>
-			<li><a href="#servicios">Servicios</a></li>
-			<li><a href="#innovacion">Innovación</a></li>
-			<li><a href="#marcas">Marcas</a></li>
-			<li><a href="#equipo">Equipo</a></li>
-			<li />
-		</ul>
-	</nav>
-</header>
+<main on:mousemove={handleMouseMove} on:mousedown={handleMouseDown} on:mouseup={handleMouseUp}>
+	<InteractiveCursor />
+	<header>
+		<nav>
+			<ul on:mousemove={handleMouseMove}>
+				<li><a href="#servicios">Servicios</a></li>
+				<li><a href="#innovacion">Innovación</a></li>
+				<li><a href="#marcas">Marcas</a></li>
+				<li><a href="#equipo">Equipo</a></li>
+				<li />
+			</ul>
+		</nav>
+	</header>
 
-<slot />
+	<slot />
+</main>
 
 <style>
-	:root {
-		--background-color: rgb(0 0 0);
-		--nav-color: rgba(80 80 80 1);
-		--nav-color-hover: rgba(255 255 255 1);
+	main {
+		--color-1: #ff6b3d;
+		--color-2: #e53645;
+		--color-3: #ffbe57;
+		--background-color: black;
+
+		background-color: var(--background-color);
+
+		position: relative;
 	}
 
 	ul {
@@ -30,9 +52,5 @@
 	ul a {
 		text-decoration: none;
 		background: transparent;
-	}
-
-	header {
-		background-color: var(--background-color);
 	}
 </style>
