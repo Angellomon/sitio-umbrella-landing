ea74cde5aeaf81bf1117923aa025ad1d5767c673
Angellomon <angelmtzdiaz@gmail.com>
2023-06-19 11:54:45 -0600

    feat(components/header): impl Header Components

---
+++ src/lib/components/header/main-header.svelte
@@ -0,0 +1,22 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="#servicios">Servicios</a></li>
+			<li><a href="#innovacion">Innovación</a></li>
+			<li><a href="#marcas">Marcas</a></li>
+			<li><a href="#equipo">Equipo</a></li>
+			<li />
+		</ul>
+	</nav>
+</header>
+
+<style>
+	ul {
+		list-style-type: none;
+	}
+
+	ul a {
+		text-decoration: none;
+		background: transparent;
+	}
+</style>
