71eb583703112b3b955d859df291a8a47af175d0
Angellomon <angelmtzdiaz@gmail.com>
2023-06-20 15:11:34 -0600

    fix(styles): impl correct color + fix weir margin bug

--- src/lib/components/interactive-cursor/interactive-cursor.svelte
+++ src/lib/components/interactive-cursor/interactive-cursor.svelte
@@ -10,13 +10,14 @@
 	svg {
 		width: 100%;
 		height: 100%;
-		margin: -8px;
+		/* margin: -8px; */
 		position: absolute;
 		z-index: -100;
+		background-color: var(--background-color);
 	}
 
 	circle {
 		position: absolute;
-		fill: var(--color-1);
+		fill: var(--accent-orange);
 	}
 </style>
